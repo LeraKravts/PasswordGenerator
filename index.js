a6d0a8b7653bc602cabf44d6b5651d8f6f2d782d
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2023-10-03 13:53:04 +0200

    add files

---
+++ index.js
@@ -0,0 +1,55 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passwordOne = ""
+let passwordTwo = ""
+let passwordField1 = document.getElementById("password-1")
+let passwordField2 = document.getElementById("password-2")
+
+
+
+function getRandomSymbol() {
+    let i = Math.floor(Math.random() * characters.length)
+    return characters[i]
+    }
+
+
+function getCharacters() {
+    let charactersSetOne = []
+    let charactersSetTwo = []
+    
+    for (let i = 0; i <= 15; i++){
+        charactersSetOne.push(getRandomSymbol())
+    }
+    for (let i=0; i<=15; i++) {
+        passwordOne += charactersSetOne[i]
+    }
+    
+     for (let i = 0; i <= 15; i++){
+        charactersSetTwo.push(getRandomSymbol())
+    }
+    for (let i=0; i<=15; i++) {
+        passwordTwo  += charactersSetOne[i]
+    }
+    
+    passwordField1.textContent = passwordOne
+    passwordField2.textContent = passwordTwo
+    
+}
+
+
+   
+    
+
+function generateTwopasswords() {
+    passwordOne = ""
+    passwordTwo = ""
+    getCharacters()
+
+}
+
+
+// console.log(getCharacters())
+
+
