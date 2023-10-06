413cb61d684ea56966ed77823052511894b2415a
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2023-10-06 23:45:08 +0200

    making better

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-
-let passwordOne = ""
-let passwordTwo = ""
 const passwordField1 = document.getElementById("password-1")
 const passwordField2 = document.getElementById("password-2")
 
-
+let passwordOne = ""
+let passwordTwo = ""
 
 function getRandomSymbol() {
     let i = Math.floor(Math.random() * characters.length)
